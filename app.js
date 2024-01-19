e0da76e7ce3f07799a9d066220e717e893a783f9
IEsquivel2000 <iesquivel200010@gmail.com>
2024-01-19 12:50:02 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 
 let numSecreto = Math.floor(Math.random() * 10) + 1; // Returns a random integer from 1 to 10:
-let numUser = prompt("Me indicas un numero por favor: ");
+console.log(numSecreto);
 
+let numUser = prompt("Me indicas un numero por favor: ");
 console.log(numUser);
 
 if (numUser == numSecreto){
