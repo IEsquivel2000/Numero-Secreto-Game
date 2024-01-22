f8683b5a3e1112183dabbda3dcc58e14118faba5
IEsquivel2000 <iesquivel200010@gmail.com>
2024-01-22 12:52:51 -0500

    Update app.js

--- app.js
+++ app.js
@@ -7,17 +7,18 @@ console.log(numSecret);
 let correct = false;
 
 let intentos = 1; //Intentos del jugador
+let intentosMax = 5// Numero de intentos maximo
 
+alert(`Bienvenido, tienes ${intentosMax} intentos`)
 while(correct == false) {
     
     try{
-
         let numUser = prompt("Introduzca un valor entre 0 - 10: ");
         console.log(numUser);
 
         if(numUser == numSecret & !isNaN(numUser) && numUser != null && numUser != "") {
             //Se cumplio la condicion
-            alert(`Numero correcto!, el numero fue: ${numSecret}. Se utilizo una cantidad de ${intentos} intentos`);
+            alert(`Numero correcto!, el numero fue: ${numSecret}. Se utilizo una cantidad de ${intentos} ${intentos == 1 ? 'intento':'intentos'}`);
             correct = true;
         }else if(numUser > 10 | numUser < 0){
             alert("Valor ingresado no es valido, intenta nuevamente");
@@ -30,9 +31,13 @@ while(correct == false) {
                 //No se cumplio la condicion
                 alert("El numero introducido es muy bajo, intenta nuevamente");
             }
+            alert(`Tienes ${intentosMax-intentos} intentos restantes`);
             intentos++;
             correct = false;
-
+        }
+        if(intentos > intentosMax){
+            alert("Llegaste al maximo de intentos posibles");
+            break;
         }
     
     }catch(error){
